6232de9ba7ab261ae4959c0e8eb788bbf337c6e6
meepstertron <meepstertron@gmail.com>
2024-08-09 13:55:11 +0200

    Update test.js

--- Plugins/test.js
+++ Plugins/test.js
@@ -1,3 +1,18 @@
-window.onBodyChange = function() {
-    console.log('Hello World!');
+const targetNode = document.getElementsByTagName('body');
+
+// Options for the observer (which mutations to observe)
+const config = { attributes: true, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+const callback = (mutationList, observer) => {
+  console.log("Hello World!")
 };
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
+
+// Later, you can stop observing
+observer.disconnect();
